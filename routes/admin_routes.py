aa7048baa3b13bf68ab6d377164f13de28d1eb8f
Milena <melolinhares73@gmail.com>
2024-09-04 14:33:21 -0300

    teste commit inserir produto

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from dtos.novo_produto_dto import NovoProdutoDTO
+from dtos.excluir_produto_dto import ExcluirProdutoDTO
+from dtos.inserir_produto_dto import InserirProdutoDTO
+from models.produto_model import Produto
 from repositories.produto_repo import ProdutoRepo
 
 
@@ -13,5 +15,14 @@ async def obter_produtos():
     return produtos
 
 @router.post("/inserir_produto")
-async def inserir_produto(produto: NovoProdutoDTO):
-    pass
+async def inserir_produto(produto: InserirProdutoDTO) -> Produto:
+    novo_produto = Produto(None, produto.nome, produto.preco, produto.descricao, produto.estoque)
+    novo_produto = ProdutoRepo.inserir(novo_produto)
+    return novo_produto
+
+
+# @router.get("/excluir_produto")
+# async def excluir_produto(produto: ExcluirProdutoDTO) -> Produto:
+#     produto = ProdutoRepo.obter_um(produto.id)
+#     produto = ProdutoRepo.excluir(produto)
+#     return produto
